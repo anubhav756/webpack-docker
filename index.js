7733f07771231a8d8423724c2bdf90aeb2bb62bd
Anubhav Dhawan <anubhav.dhawan@daffodilsw.com>
2016-09-22 14:48:13 +0530

    strip down irrelevant parts

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
-
-import App from './modules/App';
-import Login from './modules/Login';
-import Register from './modules/Register';
 
 ReactDOM.render((
-	<Router history={hashHistory}>
-		<Route path="/" component={App}>
-			<Route path="/login" component={Login} />
-			<Route path="/register" component={Register} />
-		</Route>
-	</Router>
+	<div>Hello</div>
 ), document.getElementById('app'))
--- modules/NavLink.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router';
-
-export default React.createClass({
-    render: function () {
-        return <Link {...this.props} activeClassName="active" />;
-    }
-});
